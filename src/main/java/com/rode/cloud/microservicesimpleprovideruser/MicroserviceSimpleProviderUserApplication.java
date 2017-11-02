76477ecdb649ad1a4b155719d888fa8aae27cf58
roderickzxy <roderickzxy@qq.com>
2017-11-02 23:36:33 +0800

    add some comment

--- src/main/java/com/rode/cloud/microservicesimpleprovideruser/MicroserviceSimpleProviderUserApplication.java
+++ src/main/java/com/rode/cloud/microservicesimpleprovideruser/MicroserviceSimpleProviderUserApplication.java
@@ -3,6 +3,9 @@ package com.rode.cloud.microservicesimpleprovideruser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Test application
+ */
 @SpringBootApplication
 public class MicroserviceSimpleProviderUserApplication {
 
